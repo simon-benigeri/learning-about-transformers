8bd2d8ef413eb1f041f7c1d708d5375ebc69fd58
simon-benigeri <simon.benigeri@protonmail.ch>
2021-09-01 11:19:12 +0200

    reorganize attention

--- transformer/SubLayers.py
+++ transformer/SubLayers.py
@@ -1,38 +1,48 @@
-import math, copy, time
-import numpy as np
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
-from utils import *
+from utils import clones
 
-class ScaledDotProductAttention(nn.Module):
-    def __init__(self, temperature: float=None, attention_dropout: float=0.1):
-        super().__init__()
-        self.dropout = nn.Dropout(p=attention_dropout)
-        # self.temperature = temperature
-
-    def forward(self, query: Tensor, key: Tensor, value: Tensor, mask=None):
-        d_k = key.size(-1)
-        # matmul Q, K.T and scale by dividing by temperature (usually d_k ** 0.5)
-        # scores = torch.matmul(query, key.transpose(-2, -1)) / self.temperature
-        scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
-        if mask is not None:
-            scores = scores.masked_fill(mask == 0, -1e9)
-        attention_filter = self.dropout(F.softmax(scores, dim=-1))
-        output = torch.matmul(attention_filter, value)
-        return attention_filter, output
-
+def scaled_dot_product_attention(query: Tensor,
+                                 key: Tensor,
+                                 value: Tensor,
+                                 mask: Tensor=None,
+                                 dropout: nn.Dropout=None
+                                 ) -> (Tensor, Tensor):
+    """Compute Scaled Dot Product Attention. Attention(Q,K,V) = softmax(QK.T/sqrt(d_k)) V"""
+    d_k = key.size(-1)
+    # QK.T/sqrt(d_k)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    # apply optional mask
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    # softmax scores
+    attention_filter = F.softmax(scores, dim=-1)
+    # apply optional dropout
+    if dropout is not None:
+        attention_filter = dropout(attention_filter)
+    # matmul with values
+    return torch.matmul(attention_filter, value), attention_filter
 
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, heads: int, d_model: int, dropout=0.1):
+    def __init__(self, n_heads: int, d_model: int, dropout=0.1):
         """input the model size and number of heads"""
         super().__init__()
-        assert d_model % heads == 0
+        assert d_model % n_heads == 0
         # assume d_v always equals d_k
-        self.d_k = d_model // heads
-        self.heads = heads
-        # TODO: finish this
-        # self.linear_layers = clones(nn.Linear(in_features=))
+        self.d_k = d_model // n_heads
+        self.heads = n_heads
+        self.linear_layers = clones(module=nn.Linear(in_features=d_model, out_features=d_model), N=4)
+        self.attention = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query: Tensor, key: Tensor, value: Tensor, mask: Tensor=None):
+        if mask is not None:
+            # we apply same mask to all heads
+            mask = mask.unsqueeze(1)
+        num_batches = query.size(0)
+
     pass
