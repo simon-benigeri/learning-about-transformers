7c26cf962311c155f6a69e70361394d834d3f581
simon-benigeri <simon.benigeri@protonmail.ch>
2021-09-04 19:19:16 +0200

    Update sublayers.py

--- transformer/sublayers.py
+++ transformer/sublayers.py
@@ -1,8 +1,6 @@
 """Sublayers of Encoder and Decoder layers can be found here:
-ScaledDotProductAttention,
-MultiHeadedAttention,
-LayerNorm,
-and SublayerConnection"""
+ScaledDotProductAttention, MultiHeadAttention, LayerNorm, SublayerConnection"""
+
 import math
 import torch
 import torch.nn as nn
@@ -31,8 +29,7 @@ class ScaledDotProductAttention(nn.Module):
         if self.dropout is not None:
             attention_filter = self.dropout(attention_filter)
         # matrix multiply attention filter with values
-        filtered_value = torch.matmul(attention_filter, value)
-        return filtered_value, attention_filter
+        return torch.matmul(attention_filter, value), attention_filter
 
 
 class MultiHeadAttention(nn.Module):
@@ -56,16 +53,17 @@ class MultiHeadAttention(nn.Module):
         if mask is not None:
             mask = mask.unsqueeze(1)
         n_batches = query.size(0)
-        # pass them through the pre-attention linear projection layers n_batches x d_model x (n_heads*d_k)
-        query, key, value = [ linear(x) for linear, x in zip(self.linears, (query, key, value)) ]
-        # we separate the attention heads
-        # shape of query, key, value: (n_batches, d_model, d_model) -> (n_batches, d_model, heads, d_k)
-        # shape of query, key, value: -> (n_batches, d_model, heads, d_k) -> (n_batches, heads, d_model, d_k)
-        query = query.view(n_batches, -1, self.n_heads, self.d_k).transpose(1, 2)
-        key = key.view(n_batches, -1, self.n_heads, self.d_k).transpose(1, 2)
-        value = value.view(n_batches, -1, self.n_heads, self.d_k).transpose(1, 2)
+        # 1. pass query, key, value through the pre-attention linear projection layers
+        # 2. separate the attention heads
+        # shape of query, key, value:
+        # (n_batches, d_model, d_model) -> (n_batches, d_model, heads, d_k) -> (n_batches, heads, d_model, d_k)
+        query, key, value = [
+            linear(x).view(n_batches, -1, self.n_heads, self.d_k).transpose(1, 2)
+            for linear, x in zip(self.linears, (query, key, value))
+        ]
         # Apply attention on all the projected vectors in batch.
         filtered_value, attention_filter = self.attention.forward(query=query, key=key, value=value, mask=mask)
-        # concatenate outputs using a view and apply the final linear
+        # concatenate attention value outputs
         filtered_value = filtered_value.transpose(1, 2).contiguous().view(n_batches, -1, self.n_heads * self.d_k)
+        # apply final output layer (n_batches, d_model, d_k)
         return self.linears[-1](filtered_value)
