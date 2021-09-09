f7f43cd73a08cbd8644dda971c0d448f335edaa9
simon-benigeri <simon.benigeri@protonmail.ch>
2021-09-09 13:41:26 +0200

    update imports

--- transformer/encoder_decoder.py
+++ transformer/encoder_decoder.py
@@ -3,8 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from encoder import Encoder
-from decoder import Decoder
+from encoder import Encoder, EncoderLayer
+from decoder import Decoder, DecoderLayer
 from models import Embeddings, PositionalEncoding
 
 class EncoderDecoder(nn.Module):
